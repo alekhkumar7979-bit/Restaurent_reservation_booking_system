e34ab1c684845307f411b6d571cfa7d003854d08
Alekh Kumar <alekhkumar7979@gmail.com>
2026-03-24 01:05:42 +0530

    Added project with gitignore

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">MOTI_MAHAL</div>
+          <div className="right">
+            <p>K-Road,Bistupur,Jamshedpur</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By ALEKH_KUMAR</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By ALEKH KUMAR.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
